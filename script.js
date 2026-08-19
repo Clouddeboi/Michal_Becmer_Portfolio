51870368413df8eaf2bbc7964317941137e7c6c8
Clouddeboi <michalcloud2003@gmail.com>
2026-08-19 23:39:12 +0100

    📄 Added CV

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const config = {
             <br>
             <p>With a strong focus on programming and systems development, I enjoy building robust, scalable gameplay systems and exploring the technical side of game development. I’m driven by a desire to continuously learn, experiment with new technologies, and create polished experiences that bring creative ideas to life..</p>
         `,
-        resumeLink: "assets/resume/placeholder-cv.pdf"
+        resumeLink: "assets/resume/Michal_Becmer_CV.pdf"
     },
     socials: [
         { name: "GitHub", icon: "fab fa-github", url: "https://github.com/Clouddeboi" },
